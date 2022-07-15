d9033bb455144ec494e3f043ce3b4bebd556ca27
Christian <christianyip92@gmail.com>
2022-07-15 09:41:53 -0700

    Add .color styling and start on hover event.

--- script.js
+++ script.js
@@ -5,9 +5,16 @@ const container = document.querySelector('.container');
 // creates divBox and appends to container 16 times
 function loop() {
     for (i=0; i<256; i++) {
-        let divBox = document.createElement('div');
+        divBox = document.createElement('div');
         container.append(divBox);
     }
 }
 
 loop();
+ 
+const divBoxes = document.querySelectorAll('.container > div');
+// // add color to divBoxes on hover
+// divBoxes.addEventListener('mouseover', function addColor () {
+//     divBox.classList.add('addColor');
+// })
+console.log(divBoxes);
